17f7cb636850ddde3c8e9280ec13c08669ca3a63
Harshad Kasar <125277919+Harshadkasar14@users.noreply.github.com>
2025-06-22 15:06:29 +0530

    Delete 01-rest-api-starting-code directory

--- 01-rest-api-starting-code/src/main/java/com/restfulwebservices/todoEntity/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restfulwebservices.todoEntity;
-
-import com.restfulwebservices.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-    List<Todo> findByUser(User user);
-}
